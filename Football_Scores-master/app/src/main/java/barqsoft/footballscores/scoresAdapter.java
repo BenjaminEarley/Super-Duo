3d105979e57f80442ec87b2fba8b2a7d2ac06e46
Benjamin Earley <BenjaminEarley92@gmail.com>
2015-09-21 21:40:20 -0400

    Added Content Description to team images

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -52,9 +52,11 @@ public class scoresAdapter extends CursorAdapter
         mHolder.match_id = cursor.getDouble(COL_ID);
         mHolder.home_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_HOME)));
+        mHolder.home_crest.setContentDescription(cursor.getString(COL_HOME));
         mHolder.away_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_AWAY)
         ));
+        mHolder.away_crest.setContentDescription(cursor.getString(COL_AWAY));
         //Log.v(FetchScoreTask.LOG_TAG,mHolder.home_name.getText() + " Vs. " + mHolder.away_name.getText() +" id " + String.valueOf(mHolder.match_id));
         //Log.v(FetchScoreTask.LOG_TAG,String.valueOf(detail_match_id));
         LayoutInflater vi = (LayoutInflater) context.getApplicationContext()
