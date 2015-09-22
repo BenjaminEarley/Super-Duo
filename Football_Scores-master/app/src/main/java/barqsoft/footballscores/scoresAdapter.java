06e7c598436e9d3bb7ea7ddec41f928adb354193
Benjamin Earley <BenjaminEarley92@gmail.com>
2015-09-21 22:34:32 -0400

    Set Content Description for cards and share button

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -48,15 +48,17 @@ public class scoresAdapter extends CursorAdapter
         mHolder.home_name.setText(cursor.getString(COL_HOME));
         mHolder.away_name.setText(cursor.getString(COL_AWAY));
         mHolder.date.setText(cursor.getString(COL_MATCHTIME));
-        mHolder.score.setText(Utilies.getScores(cursor.getInt(COL_HOME_GOALS),cursor.getInt(COL_AWAY_GOALS)));
+        mHolder.score.setText(Utilies.getScores(cursor.getInt(COL_HOME_GOALS), cursor.getInt(COL_AWAY_GOALS)));
         mHolder.match_id = cursor.getDouble(COL_ID);
         mHolder.home_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_HOME)));
-        mHolder.home_crest.setContentDescription(cursor.getString(COL_HOME));
         mHolder.away_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_AWAY)
         ));
-        mHolder.away_crest.setContentDescription(cursor.getString(COL_AWAY));
+        String holderContentDescription = mHolder.home_name.getText() + " Vs. " + mHolder.away_name.getText()
+                + " with a score of " + cursor.getInt(COL_HOME_GOALS) + " to " + cursor.getInt(COL_AWAY_GOALS)
+                + " at " + cursor.getString(COL_MATCHTIME);
+        view.setContentDescription(holderContentDescription);
         //Log.v(FetchScoreTask.LOG_TAG,mHolder.home_name.getText() + " Vs. " + mHolder.away_name.getText() +" id " + String.valueOf(mHolder.match_id));
         //Log.v(FetchScoreTask.LOG_TAG,String.valueOf(detail_match_id));
         LayoutInflater vi = (LayoutInflater) context.getApplicationContext()
@@ -81,9 +83,18 @@ public class scoresAdapter extends CursorAdapter
                 {
                     //add Share Action
                     context.startActivity(createShareForecastIntent(mHolder.home_name.getText()+" "
-                    +mHolder.score.getText()+" "+mHolder.away_name.getText() + " "));
+                            +mHolder.score.getText()+" "+mHolder.away_name.getText() + " "));
                 }
             });
+            share_button.setContentDescription("Share match between " + cursor.getString(COL_HOME)
+                    + " and " + cursor.getString(COL_AWAY) + " with a score of "
+                    + cursor.getInt(COL_HOME_GOALS) + " to " + cursor.getInt(COL_AWAY_GOALS));
+
+            String detailsContainerDescription = "with a match day of " + cursor.getInt(COL_MATCHDAY);
+            if (!league.getText().equals("Not known League Please report")) {
+                detailsContainerDescription += "in League " + league.getText();
+            }
+            view.setContentDescription(holderContentDescription + " " + detailsContainerDescription);
         }
         else
         {
