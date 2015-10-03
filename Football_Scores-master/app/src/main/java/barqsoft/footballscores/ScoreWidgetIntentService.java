3dc95b41ddba7b75a85963435f66537cf792090c
bearley@outsitenetworks.com <bearley@outsitenetworks.com>
2015-10-03 19:48:23 -0400

    Finally getting some data

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/ScoreWidgetIntentService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/ScoreWidgetIntentService.java
@@ -6,24 +6,25 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import barqsoft.footballscores.service.myFetchService;
 
-public class ScoreWidgetIntentService extends IntentService implements LoaderManager.LoaderCallbacks<Cursor> {
+public class ScoreWidgetIntentService extends IntentService implements Loader.OnLoadCompleteListener<Cursor> {
     private static final String[] SCORES_COLUMNS = {
             DatabaseContract.SCORES_TABLE + "." + DatabaseContract.scores_table._ID,
             DatabaseContract.scores_table.HOME_COL,
             DatabaseContract.scores_table.AWAY_COL,
             DatabaseContract.scores_table.HOME_GOALS_COL,
-            DatabaseContract.scores_table.AWAY_GOALS_COL
+            DatabaseContract.scores_table.AWAY_GOALS_COL,
+            DatabaseContract.scores_table.TIME_COL
     };
     // these indices must match the projection
     private static final int ID = 0;
@@ -31,6 +32,12 @@ public class ScoreWidgetIntentService extends IntentService implements LoaderMan
     private static final int INDEX_AWAY_COL = 2;
     private static final int INDEX_HOME_GOALS_COL = 3;
     private static final int INDEX_AWAY_GOALS_COL = 4;
+    private static final int INDEX_TIME_COL = 5;
+
+    private CursorLoader mCursorLoader;
+    public static final int SCORES_LOADER = 0;
+
+    private String[] argumentDate = new String[1];
 
     public ScoreWidgetIntentService() {
         super("ScoreWidgetIntentService");
@@ -45,34 +52,24 @@ public class ScoreWidgetIntentService extends IntentService implements LoaderMan
 
         update_scores();
 
-//        Uri ScoresWithDate = DatabaseContract.scores_table.buildScoreWithDate();
-//
-//        Cursor data = getContentResolver().query(ScoresWithDate, SCORES_COLUMNS, null,
-//                null, DatabaseContract.scores_table.DATE_COL + " DESC");
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
 
+        argumentDate[0] = mformat.format(date);
 
-    }
+        mCursorLoader = new CursorLoader(this,DatabaseContract.scores_table.buildScoreWithDate(),
+                SCORES_COLUMNS, null, argumentDate, DatabaseContract.scores_table.TIME_COL + " ASC");
+        mCursorLoader.registerListener(SCORES_LOADER, this);
+        mCursorLoader.startLoading();
 
-    private void update_scores()
-    {
-        Intent service_start = new Intent(this, myFetchService.class);
-        this.startService(service_start);
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this,DatabaseContract.scores_table.buildScoreWithDate(),
-                null,null,SCORES_COLUMNS,DatabaseContract.scores_table.DATE_COL + " DESC");
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    public void onLoadComplete(Loader<Cursor> loader, Cursor data) {
         if (data == null) {
-            showToast(this, "data is null");
             return;
         }
         if (!data.moveToFirst()) {
-            showToast(this, "no rows");
             data.close();
             return;
         }
@@ -81,12 +78,13 @@ public class ScoreWidgetIntentService extends IntentService implements LoaderMan
         String awayName = data.getString(INDEX_AWAY_COL);
         String homeGoals = data.getString(INDEX_HOME_GOALS_COL);
         String awayGoals = data.getString(INDEX_AWAY_GOALS_COL);
-        showToast(this, homeName + " " + awayName);
+        int time = data.getInt(INDEX_TIME_COL);
     }
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
+    private void update_scores()
+    {
+        Intent service_start = new Intent(this, myFetchService.class);
+        this.startService(service_start);
     }
 
     /**
